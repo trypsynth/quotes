d8fa6e28068502524345925c173a9da01d47364c
Quin <trypsynth@gmail.com>
2025-07-04 19:23:58 -0600

    Convert to use UV and Ruff.

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request
-import db
-from dotenv import load_dotenv
-from flask_basicauth import BasicAuth
 import os
+
+from dotenv import load_dotenv
+from flask import Flask, render_template, request
+from flask_basicauth import BasicAuth
+
+import db
 
 load_dotenv()
 app = Flask(__name__)
@@ -16,16 +18,16 @@ db.db.init_app(app)
 @app.route("/", methods=["GET", "POST"])
 @basic_auth.required
 def index():
-    if request.method == "POST":
-        who = request.form["who"]
-        quote = request.form["quote"]
-        context = request.form.get("context", "")
-        db.insert_quote(who, quote, context)
-    quotes = db.get_quotes()
-    return render_template("index.html", quotes=quotes)
+	if request.method == "POST":
+		who = request.form["who"]
+		quote = request.form["quote"]
+		context = request.form.get("context", "")
+		db.insert_quote(who, quote, context)
+	quotes = db.get_quotes()
+	return render_template("index.html", quotes=quotes)
 
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.setup()
-    app.run()
+	with app.app_context():
+		db.setup()
+	app.run()
--- db.py
+++ db.py
@@ -4,21 +4,21 @@ db = SQLAlchemy()
 
 
 class Quote(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    who = db.Column(db.String(255), nullable=False)
-    quote = db.Column(db.Text, nullable=False)
-    context = db.Column(db.Text, nullable=False, default="")
+	id = db.Column(db.Integer, primary_key=True)
+	who = db.Column(db.String(255), nullable=False)
+	quote = db.Column(db.Text, nullable=False)
+	context = db.Column(db.Text, nullable=False, default="")
 
 
 def setup():
-    db.create_all()
+	db.create_all()
 
 
 def insert_quote(who, quote, context):
-    new_quote = Quote(who=who, quote=quote, context=context)
-    db.session.add(new_quote)
-    db.session.commit()
+	new_quote = Quote(who=who, quote=quote, context=context)
+	db.session.add(new_quote)
+	db.session.commit()
 
 
 def get_quotes():
-    return Quote.query.all()
+	return Quote.query.all()
